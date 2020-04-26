9889913218d963e2aed665f0a62f5cb65ccd201c
Aaron <56502347+aci686@users.noreply.github.com>
2020-04-26 23:00:11 +0100

    Create notify-send-all.sh

---
+++ notify-send-all.sh
@@ -0,0 +1,17 @@
+## This script needs libnotify-bin to work as it relies on notify-send command
+## We can use this script to send desktop notifications to all users logged in using any X display
+## notify-send uses the following syntax:
+## notify-send --urgency=[low|normal|critical] --expire-time 1 --icon=[icon_file] "Subject" "Message"
+## Refer to notify-send man page for additional details
+## [icon_file] should be some located at /usr/share/icons/gnome/32x32/ and its subfolders
+
+#!/bin/bash
+PATH=/usr/bin:/bin
+
+XUSERS=($(who|grep -E "\(:[0-9](\.[0-9])*\)"|awk '{print $1$5}'|sort -u))
+for XUSER in $XUSERS; do
+	NAME=(${XUSER/(/ })
+	DISPLAY=${NAME[1]/)/}
+	DBUS_ADDRESS=unix:path=/run/user/$(id -u ${NAME[0]})/bus
+	sudo -u ${NAME[0]} DISPLAY=${DISPLAY} DBUS_SESSION_BUS_ADDRESS=${DBUS_ADDRESS} PATH=${PATH} notify-send "$@"
+done
