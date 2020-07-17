b6ddbda0182d54971a5df52c209199a53e894399
Aaron <56502347+aci686@users.noreply.github.com>
2020-07-17 05:15:22 +0100

    Update notify-send-all.sh

--- notify-send-all.sh
+++ notify-send-all.sh
@@ -1,11 +1,18 @@
-## This script needs libnotify-bin to work as it relies on notify-send command
-## We can use this script to send desktop notifications to all users logged in using any X display
-## notify-send uses the following syntax:
-## notify-send --urgency=[low|normal|critical] --expire-time 1 --icon=[icon_file] "Subject" "Message"
-## Refer to notify-send man page for additional details
-## [icon_file] should be some located at /usr/share/icons/gnome/32x32/ and its subfolders
-
 #!/bin/bash
+
+# This script needs libnotify-bin to work as it relies on notify-send command
+# We can use this script to send desktop notifications to all users logged in using any X display
+# notify-send uses the following syntax:
+# notify-send --urgency=[low|normal|critical] --expire-time 1 --icon=[icon_file] "Subject" "Message"
+# Refer to notify-send man page for additional details
+# [icon_file] should be some located at /usr/share/icons/gnome/32x32/ and its subfolders#
+#
+
+__author__="Aaron Castro"
+__author_email__="aaron.castro.sanchez@outlook.com"
+__copyright__="Aaron Castro"
+__license__="MIT"
+
 PATH=/usr/bin:/bin
 
 XUSERS=($(who|grep -E "\(:[0-9](\.[0-9])*\)"|awk '{print $1$5}'|sort -u))
